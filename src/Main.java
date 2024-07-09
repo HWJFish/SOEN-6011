16241432db424203c1c9be5e08bfaa9f27f3d366
HWJFish <wwc641810324@gmail.com>
2024-07-08 23:26:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to F8 demo: standard deviation function" +
+                "\nPlease enter number in integer or double, split by space or tab; Press Enter to finish:");
+        Scanner scanner=new Scanner(System.in);
+        String inputString=scanner.nextLine();
+        String[] inputArray=inputString.split("[\\s,]+");
+        try{
+            double[] numbers=new double[inputArray.length];
+            for(int i=0;i<inputArray.length;i++){
+                numbers[i]=Double.parseDouble(inputArray[i]);
+            }
+
+            System.out.println(getStandardDeviation(numbers));
+        }catch (Exception e){
+            System.out.println("There are something wrong with the input, please check if the input type or number is correct");
+        }
+    }
+    public static double getStandardDeviation(double[] input){
+        double sum=0;
+        for (double num:input){
+            sum+=num;
+        }
+        double average= sum/input.length;
+        double sumOfSquare= 0;
+        for (double num:input){
+            sumOfSquare+=(average-num)*(average-num);
+        }
+        double standardDeviation=Math.sqrt(sumOfSquare/input.length);
+        return standardDeviation;
+    }
+}
