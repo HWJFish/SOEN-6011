7db86de0657e9788b83945e90be6e61579f2e670
HWJFish <wwc641810324@gmail.com>
2024-07-22 22:22:32 -0400

    Update view for better usability

--- GUI/src/main/java/com/example/gui/Calculation.java
+++ GUI/src/main/java/com/example/gui/Calculation.java
@@ -1,7 +1,12 @@
 package com.example.gui;
 
 public class Calculation {
-    // Core function for calculation
+
+    /**
+     * Core function for calculation of standard deviation
+     * @param input an array of double for numbers need to calculate
+     * @return the standard deviation from the given input
+     */
     public static double getStandardDeviation(double[] input){
         double sum=0;
         for (double num:input){
@@ -16,8 +21,14 @@ public class Calculation {
         return standardDeviation;
     }
 
+    /**
+     * Function to handle inputString
+     * @param inputString the String from keyboard or textarea
+     * @return the standard deviation from the given input
+     * @throws Exception Mostly NumberFormatException when input have wrong input type or empty input
+     */
     public static double getStandardDeviationWithString(String inputString) throws Exception{
-        String[] inputArray=inputString.split("[\\s,]+");
+        String[] inputArray=inputString.split("[\\n\\s,]+");
         double[] numbers=new double[inputArray.length];
         for(int i=0;i<inputArray.length;i++){
             numbers[i]=Double.parseDouble(inputArray[i]);
--- GUI/src/main/java/com/example/gui/StandardDeviationGUIMain.java
+++ GUI/src/main/java/com/example/gui/StandardDeviationGUIMain.java
@@ -11,7 +11,7 @@ public class StandardDeviationGUIMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StandardDeviationGUIMain.class.getResource("mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Standard Deviation");
         stage.setScene(scene);
         stage.show();
