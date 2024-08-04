0c2c2047913b132fd20d35d82c6e1a27193c860f
HWJFish <wwc641810324@gmail.com>
2024-08-04 08:14:28 -0400

    Add design principles and accessible feature

--- GUI/src/main/java/com/example/gui/MainController.java
+++ GUI/src/main/java/com/example/gui/MainController.java
@@ -2,6 +2,8 @@ package com.example.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class MainController {
@@ -10,12 +12,37 @@ public class MainController {
   private TextArea inputText;
   @FXML
   private TextArea outputText;
+  @FXML
+  private Label OutputInstructionText;
+  @FXML
+  Label welcomeMessage;
+  @FXML
+  Label instruction;
+  @FXML
+  Button calculateButton;
+  @FXML
+  public void initialize() {
+    welcomeMessage.setAccessibleText("Welcome message");
+    welcomeMessage.setAccessibleHelp("A message for welcome");
+    instruction.setAccessibleText("Instruction text");
+    instruction.setAccessibleHelp("Enter numbers separated by spaces, tabs, or newlines");
+    inputText.setAccessibleText("Input box");
+    inputText.setAccessibleHelp("Enter your input here");
+    calculateButton.setAccessibleText("Calculate Button");
+    calculateButton.setAccessibleHelp("Click this button to calculate");
+
+  }
 
   @FXML
   protected void onCalculateButtonClick() {
     try {
+      OutputInstructionText.setText("Calculating...");
+      OutputInstructionText.setVisible(true);
       double output = Calculation.getStandardDeviationWithString(inputText.getText());
+      OutputInstructionText.setText("Result:");
+      OutputInstructionText.setStyle("");
       outputText.setText(output + "");
+      outputText.setVisible(true);
     } catch (NumberFormatException e) {
       showErrorMessage(
           "You have non-numeric or empty input entered please double check!\n" + e.getMessage());
@@ -26,6 +53,11 @@ public class MainController {
   }
 
   private void showErrorMessage(String errorMessage) {
+    OutputInstructionText.setText("Error: "+errorMessage);
+    OutputInstructionText.setVisible(true);
+    OutputInstructionText.setStyle("-fx-text-fill: red;");
+    outputText.setText("");
+    outputText.setVisible(false);
     Alert alert = new Alert(Alert.AlertType.ERROR);
     alert.setTitle("Error Alert");
     alert.setHeaderText(null);
